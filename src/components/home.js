b8164b58f8ec79b58967cb3b890ec262395c3cf8
KeithNorland <norlandenator@gmail.com>
2018-12-06 15:05:01 -0700

    commit

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Library from './library/library';
 import Schedule from './schedule/schedule';
 
- class Home extends Component {
+class Home extends Component {
   render() {
     return (
       <div className="home">
